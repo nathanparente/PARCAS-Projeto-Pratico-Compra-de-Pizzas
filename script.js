48664ed36d39a180a90dda5111bdddce90daee58
Nathan Parente <nathan.parente22@gmail.com>
2022-05-18 09:51:21 -0300

    Aula03

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     // preencher as informações do meu PIZZA TEMPLATE com os items do array
 
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = item.price;
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
