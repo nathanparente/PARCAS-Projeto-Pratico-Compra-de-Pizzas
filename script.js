4bdd17257b19c07aff972587fe3604a94f8d5663
Nathan Parente <nathan.parente22@gmail.com>
2022-05-19 15:35:16 -0300

    Aula13-14

--- script.js
+++ script.js
@@ -102,13 +102,29 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     closeModal();
 });
 
+//MENU-MOBILE OPEN CLOSE AREA
+c('.menu-openner').addEventListener('click',()=>{
+    if(cart.length > 0){
+        c('aside').style.left = '0';
+    }
+});
+c('.menu-closer').addEventListener('click',()=>{
+    c('aside').style.left = '100vw';
+})
+
 //CART AREA
 function updateCart(){
+    c('.menu-openner span').innerHTML = cart.length;
     if(cart.length > 0){
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart){
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
             let cartItem = c('.models .cart--item').cloneNode(true);
             
             let pizzaSizeName; //= cart[i].size;
@@ -127,18 +143,38 @@ function updateCart(){
                 break;
             }
             let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
-
-
-
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
-
-
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                }else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
 
             c('.cart').append(cartItem);
         };
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     } else {
         c('aside').classList.remove('show');
+        c('aside').style.left ='100vw';
     }
 };
+
+//ALERT PEDIDO FINALIZADO
+c('.cart--finalizar').addEventListener('click', ()=>{
+    alert("Pedido Finalizado, volte sempre!!");
+})
